02d910228043b3205cd33eb66c9ed1e0ecd2e6e6
ashkan <ashkan@gmail.com>
2026-06-30 09:57:59 +0330

    feat: add basic home, about, and contact pages with routes and views

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def home(requests):
+    return render(requests, "index.html")
+
+def about(requests):
+    return render(requests, "about.html")
+
+def contact(requests):
+    return render(requests, "contact.html")
